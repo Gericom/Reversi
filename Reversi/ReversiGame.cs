30092ee9e6fb965165050f0dabf77767ae074f3e
Opiou <rubenrobert01@gmail.com>
2016-10-12 14:25:30 +0200

    enclosedfields and ifs and elses
    WHOOP

--- Reversi/ReversiGame.cs
+++ Reversi/ReversiGame.cs
@@ -17,7 +17,7 @@ namespace Reversi
                 Player2
             }
 
-            public FieldContent Content { get; set; }
+            public FieldContent Content { get; set; } = FieldContent.Empty;
         }
 
         private ReversiField[,] mFields;
@@ -30,13 +30,45 @@ namespace Reversi
             mFields = new ReversiField[BoardSize, BoardSize];
         }
 
-        public ReversiField[] GetEnclosedFields(int x, int y)
+        public ReversiField[] GetEnclosedFields(int x, int y, ReversiField.FieldContent player)
         {
-            if (x < 0 || y < 0 || x >= BoardSize || y >= BoardSize)
+            if (x < 0 || y < 0 || x >= BoardSize || y >= BoardSize || player == ReversiField.FieldContent.Empty)
                 return new ReversiField[0];
             List<ReversiField> enclosedFields = new List<ReversiField>();
 
+            enclosedFields.AddRange(GetEnclosedFieldsInDirection(x, y, 0, -1, player));
+            enclosedFields.AddRange(GetEnclosedFieldsInDirection(x, y, 1, -1, player));
+            enclosedFields.AddRange(GetEnclosedFieldsInDirection(x, y, 1, 0, player));
+            enclosedFields.AddRange(GetEnclosedFieldsInDirection(x, y, 1, 1, player));
+            enclosedFields.AddRange(GetEnclosedFieldsInDirection(x, y, 0, 1, player));
+            enclosedFields.AddRange(GetEnclosedFieldsInDirection(x, y, -1, 1, player));
+            enclosedFields.AddRange(GetEnclosedFieldsInDirection(x, y, -1, 0, player));
+            enclosedFields.AddRange(GetEnclosedFieldsInDirection(x, y, -1, -1, player));
+
+
             return enclosedFields.ToArray();
+        }
+
+        private ReversiField[] GetEnclosedFieldsInDirection(int x, int y, int dx, int dy, ReversiField.FieldContent player)
+        {
+            if ( x < 0 || y < 0 || x >= BoardSize || y >= BoardSize || player == ReversiField.FieldContent.Empty )
+                return new ReversiField[0];
+
+            List<ReversiField> enclosedFields = new List<ReversiField>();
+            while ( x > 0 && y > 0 && x < BoardSize && y < BoardSize )
+            {
+                ReversiField field = mFields[y,x];
+                if ( field.Content == player )
+                    return enclosedFields.ToArray();
+                else if ( field.Content == ReversiField.FieldContent.Empty )
+                    break;
+                enclosedFields.Add(field);
+                x += dx;
+                y += dy;
+            
+                  
+            }
+            return new ReversiField[0];
         }
 
 
